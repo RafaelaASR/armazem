6207fab0a6e48d3c49ca18722c7f5ebd8bca7af0
Rafaela de Abreu <rafaelaasr.ec@gmail.com>
2025-09-22 22:36:36 -0300

    [ADD] Adicionando input para inserir origem, destino e opções de médoto de busca

--- main.py
+++ main.py
@@ -1,39 +1,170 @@
+import tkinter as tk
+from tkinter import ttk
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-# Carregar imagem
-img = mpimg.imread("./assets/teste.png")
+from collections import deque
+from Node import Node
 
-# Criar grid fictício (10x10)
-# 0 = livre, 9 obstáculo
-grid = np.zeros((10, 10))
-grid[-9, 4] = 9
-grid[6, 2:7] = 9
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-path = [
-    (4, 5),
-    (6, 7),
-    (7, 9),
-    (8, 6),
-    (8, 8),
-    (2,7),
-    (3,11),
-]
+class main(object):
 
-fig, ax = plt.subplots()
+    # Carregar imagem do grid
+    img = mpimg.imread("./assets/teste.png")
 
-plt.imshow(img, extent=[0, 10, 0, 10])  # fundo com imagem
-plt.imshow(grid, cmap="tab20c", alpha=0.5, extent=[0, 10, 0, 10])  # grid semi-transparente
+    #--------------------------------------------------------------------------
+    # SUCESSORES PARA GRID
+    #--------------------------------------------------------------------------
+    # SUCESSORES PARA GRID (LISTA DE ADJACENCIAS)
+    def sucessores_grid(self,st,nx,ny,mapa):
+        f = []
+        x, y = st[0], st[1]
+        # DIREITA
+        if y+1<ny:
+            if mapa[x][y+1]==0:
+                suc = []
+                suc.append(x)
+                suc.append(y+1)
+                f.append(suc)
+        # ESQUERDA
+        if y-1>=0:
+            if mapa[x][y-1]==0:
+                suc = []
+                suc.append(x)
+                suc.append(y-1)
+                f.append(suc)
+        # ABAIXO
+        if x+1<nx:
+            if mapa[x+1][y]==0:
+                suc = []
+                suc.append(x+1)
+                suc.append(y)
+                f.append(suc)
+        # ACIMA
+        if x-1>=0:
+            if mapa[x-1][y]==0:
+                suc = []
+                suc.append(x-1)
+                suc.append(y)
+                f.append(suc)
 
-y,x = zip(*path)
-ax.plot(x, y, color="red", linewidth=2)
-ax.scatter(x, y, color="blue", s=50)
+        return f
 
-ax.scatter(x[0], y[3], color="green", s=100, label="Início")
-ax.scatter(x[1], y[3], color="purple", s=100, label="Fim")
+    #--------------------------------------------------------------------------
+    # BUSCA EM AMPLITUDE
+    #--------------------------------------------------------------------------
+    def amplitude(self,inicio,fim,nx,ny,mapa):  # grid
+        # Finaliza se início for igual a objetivo
+        if inicio == fim:
+            return [inicio]
+        
+        # GRID: transforma em tupla
+        t_inicio = tuple(inicio)   # grid
+        t_fim = tuple(fim)         # grid
+        
+        # Lista para árvore de busca - FILA
+        fila = deque()
 
-ax.legend()
+        # Inclui início como nó raíz da árvore de busca
+        raiz = Node(None,t_inicio,0,None,None)  # grid
+        fila.append(raiz)
 
-plt.grid(True, color="white")  # linhas de grid
-plt.show()
+        # Marca início como visitado
+        visitado = {tuple(inicio): raiz}    # grid
+        
+        while fila:
+            # Remove o primeiro da FILA
+            atual = fila.popleft()
+        
+            # Gera sucessores a partir do grid
+            filhos = self.sucessores_grid(atual.estado,nx,ny,mapa) # grid
+
+            for novo in filhos:
+                t_novo = tuple(novo)       # grid
+                if t_novo not in visitado: # grid
+                    filho = Node(atual,t_novo,atual.v1 + 1,None,None) # grid
+                    fila.append(filho)
+                    visitado[t_novo] = filho # grid
+                    
+                    # Verifica se encontrou o objetivo
+                    if t_novo == t_fim:    # grid
+                        return self.exibirCaminho(filho)
+        return None
+
+
+    # Criar grid fictício (10x10)
+    # 0 = livre, 9 obstáculo
+    grid = np.zeros((10, 10))
+    grid[-9, 4] = 9
+    grid[6, 2:7] = 9
+
+    path = [
+        (4, 5),
+        (6, 7),
+        (7, 9),
+        (8, 6),
+        (3, 8),
+        (2, 7),
+        (3, 2),
+    ]
+
+    # --------------------------
+    # CRIA JANELA
+    # --------------------------
+    root = tk.Tk()
+    root.title("Buscas")
+
+    origem_label = tk.Label(root, text="Selecione a origem:")
+    origem_label.pack()
+
+    origem_combobox = ttk.Combobox(root, values=["0", "1"])
+    origem_combobox.pack()
+
+    destino_label = tk.Label(root, text="Selecione o destino:")
+    destino_label.pack()
+
+    destino_combobox = ttk.Combobox(root, values=["0", "1"])
+    destino_combobox.pack()
+
+    metodo_label = tk.Label(root, text="Selecione um método:")
+    metodo_label.pack()
+
+    metodo_combobox = ttk.Combobox(root, values=["AMPLITUDE"])
+    metodo_combobox.pack()
+
+    def metodosBusca(metodo_combobox, origem_combobox, destino_combobox):
+        option = metodo_combobox.get()
+        inicio = origem_combobox.get()
+        fim = destino_combobox.get()
+
+        if option == "AMPLITUDE":
+            caminho = amplitude(self, inicio, fim, 10, 10, grid)
+    #    elif(option = "PROFUNDIDADE"):
+
+    #    elif(option = "PROFUNDIDADE LIMITADA"):
+
+    #    elif(option = "APROFUNDAMENTO INTERATIVO"):
+
+    #    else(option = "BIDIRECIONAL"):
+
+    #    elif(option = "CUSTO UNIFORME"):
+    #    elif(option = "GREEDY"):
+    #    elif(option = "A*"):
+    #    else(option = "AIA*"):
+
+
+    plot_button = tk.Button(root, text="Enviar", command=metodosBusca(metodo_combobox, origem_combobox, destino_combobox))
+    plot_button.pack()
+
+    fig, ax = plt.subplots()
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.get_tk_widget().pack()
+
+    root.mainloop()
+    #plt.imshow(img, extent=[0, 10, 0, 10])  # fundo com imagem
+    #plt.imshow(grid, cmap="tab20c", alpha=0.5, extent=[0, 10, 0, 10])  # grid semi-transparente
+
+   
